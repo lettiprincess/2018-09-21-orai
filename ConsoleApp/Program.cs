90f03f3d93402eb62a71bd70a145d0c9d39ec566
Petrik Lajos Szg <info@petrik.hu>
2018-09-21 15:40:38 +0200

    Harmadik rész (minimum és maximum)

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -12,22 +12,47 @@ namespace ConsoleApp
         {
             int bekertSzam;
             
-
+            //Darab bekérése
             Console.WriteLine("Kérem adja meg hány számot akar megadni: ");
             int bekertDb = Convert.ToInt32(Console.ReadLine());
             int[] tomb = new int[bekertDb];
 
+            //Tömb feltöltése
             for (int i = 0; i < bekertDb; i++)
             {
                 Console.WriteLine("Kérem adja meg a {0}. számot: ",i+1);
                 bekertSzam = Convert.ToInt32(Console.ReadLine());
                 tomb[i] = bekertSzam;
             }
+
+            //Tömb kiiratása
             Console.WriteLine("Bekért számok: ");
             for (int i = 0; i < tomb.Length; i++)
             {
                 Console.Write(tomb[i] + ", ");
             }
+
+            //Maximum keresése
+            int max = 0;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (tomb[i] > max)
+                {
+                    max = tomb[i];
+                }
+            }
+            Console.WriteLine("A legnagyobb szám: " + max);
+
+            //Minimum keresése
+            int min = tomb[0];
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (tomb[i] < min)
+                {
+                    min = tomb[i];
+                }
+            }
+            Console.WriteLine("A legkisebb szám: " + min);
 
             Console.ReadLine();
 
